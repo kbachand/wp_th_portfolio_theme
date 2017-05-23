ae68fd97b1cd8d8e1b05ef23c8142a8bda78a45e
kbachand <keithbachand14@gmail.com>
2017-05-22 21:05:45 -0400

    made nav menu dynamic

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
 
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+   register_nav_menus(
+    array(
+      'primary_menu' => __( 'Primary Menu' )
+      )
+    );
+}
+add_action( 'init', 'register_theme_menus' );
+
 function wpth_theme_styles() {
-    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
-    //wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'google_font', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+   wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+   //wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+   wp_enqueue_style( 'google_font', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
 }
 add_action( 'wp_enqueue_scripts', 'wpth_theme_styles' );
 
